401472e043e90425acf7c6828ca3486e2a13962c
JasonWuGenius <2480629967@qq.com>
2019-06-21 12:28:01 +0800

    20190621

--- OfferHeading_LinkedList/FindFirstCommonNodeSolution.java
+++ OfferHeading_LinkedList/FindFirstCommonNodeSolution.java
@@ -8,27 +8,42 @@ package OfferHeading_LinkedList;
 */
 public class FindFirstCommonNodeSolution {
     public static ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        
-    }
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(2);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(4);
-        ListNode node6 = new ListNode(5);
-        ListNode node7 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node7;
-
-        System.out.println("Before:");
-        PrintList.printFromHeadToTail(node1);
-        System.out.println("After:");
-        PrintList.printFromHeadToTail(deleteDuplication(node1));
-        
+        if(pHead1 == null || pHead2 == null){
+            return null;
+        }
+        ListNode pre1 = pHead1;
+        ListNode pre2 = pHead2;
+        int len1 = 0;
+        int len2 = 0;
+        while(pre1 != null){
+            pre1 = pre1.next;
+            len1++;
+        }
+        while(pre2 != null){
+            pre2 = pre2.next;
+            len2++;
+        }
+        if(len1 - len2 > 0){
+            int span = len1 - len2;
+            while(span > 0){
+                pHead1 = pHead1.next;
+                span--;
+            }
+        }
+        else{
+            int span = len2 - len1;
+            while(span > 0){
+                pHead2 = pHead2.next;
+                span--;
+            }
+        }
+        while(pHead1 != null){
+            if(pHead1 == pHead2){
+                return pHead1;
+            }
+            pHead1 = pHead1.next;
+            pHead2 = pHead2.next;
+        }
+        return null;
     }
 }
